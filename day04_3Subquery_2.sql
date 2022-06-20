2f599531504e4b0d582dc6710db0570492017b3e
Omer <mrfkrtl89@gmail.com>
2022-06-20 22:14:13 +0300

    20/06/2022

--- day04_3Subquery_2.sql
+++ day04_3Subquery_2.sql
@@ -15,13 +15,11 @@ isyeri VARCHAR(20)
 INSERT INTO calisanlar VALUES(123456789, 'Ali Seker', 'Istanbul', 2500, 'Vakko');
 INSERT INTO calisanlar VALUES(234567890, 'Ayse Gul', 'Istanbul', 1500, 'LCWaikiki');
 INSERT INTO calisanlar VALUES(345678901, 'Veli Yilmaz', 'Ankara', 3000, 'Vakko');
-INSERT INTO calisanlar VALUES(456789012, 'Veli Yilmaz', 'Izmir', 1000, 'Pierre Cardin');
+INSERT INTO calisanlar VALUES(456789012, 'Veli Yilmaz', 'Ankara', 1000, 'Pierre Cardin');
 INSERT INTO calisanlar VALUES(567890123, 'Veli Yilmaz', 'Ankara', 7000, 'Adidas');
 INSERT INTO calisanlar VALUES(456789012, 'Ayse Gul', 'Ankara', 1500, 'Pierre Cardin');
 INSERT INTO calisanlar VALUES(123456710, 'Fatma Yasa', 'Bursa', 2500, 'Vakko');
-  
-  
-  
+    
 CREATE TABLE markalar
 (
 marka_id int, 
@@ -37,24 +35,16 @@ INSERT INTO markalar VALUES(103, 'LCWaikiki', 21000);
 select * from calisanlar;
 select * from markalar;
     
-
 -- SORU1: calisan sayisi 15.000’den cok olan markalarin isimlerini ve bu markada calisanlarin isimlerini ve maaşlarini listeleyin.
-select isim, maas, isyeri from calisanlar where isyeri;
 
-select marka_isim from markalar where calisan_sayisi>15000;
-
-select isim, maas, isyeri from calisanlar where isyeri in(select marka_isim from markalar where calisan_sayisi>15000);
-
+select isim, maas, isyeri from calisanlar where isyeri in (select  marka_isim from markalar where calisan_sayisi>15000);
+select  marka_isim from markalar where calisan_sayisi>15000;
 -- SORU2: marka_id’si 101’den büyük olan marka çalişanlarinin isim, maaş ve şehirlerini listeleyiniz.
-
-
+select isim, maas, sehir from calisanlar where isyeri in (select marka_isim from markalar where marka_id>101);
 
 -- SORU3: Ankara’da calisani olan markalarin marka id'lerini ve calisan sayilarini listeleyiniz.
 
-
-
-
-
+select marka_id, calisan_sayisi from markalar where marka_isim in(select isyeri from calisanlar where sehir='Ankara' );
 
   
 /* ===================== AGGREGATE METOT KULLANIMI ===========================
@@ -64,15 +54,19 @@ select isim, maas, isyeri from calisanlar where isyeri in(select marka_isim from
 ==============================================================================*/   
       
 -- SORU4: Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin toplam maaşini listeleyen bir Sorgu yaziniz.
-
-
+select marka_isim, calisan_sayisi,(select sum(maas) from calisanlar where isyeri=marka_isim)as toplam_maas from markalar;
+-- as ile toplam maasa sutununa isim veriyoruz bu islem as olmadan da yapilir ve calisir
  
 -- SORU5: Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin ortalama maaşini listeleyen bir Sorgu yaziniz.
 
-
+select marka_isim,calisan_sayisi,(select avg(maas) from calisanlar where isyeri=marka_isim) as ortalama_maas from markalar;
+select marka_isim,calisan_sayisi,(select round(avg(maas)) from calisanlar where isyeri=marka_isim) as ortalama_maas from markalar; -- ortalamayi uyvarlar
+select marka_isim,calisan_sayisi,(select round(avg(maas),2) from calisanlar where isyeri=marka_isim) as ortalama_maas from markalar; -- ortalamayi uyvarlar ve virgulden sonra 2 basamak kullanir
  
 -- SORU6: Her markanin ismini, calisan sayisini ve o markaya ait calisanlarin maksimum ve minumum maaşini listeleyen bir Sorgu yaziniz.
 
+select marka_isim,calisan_sayisi,(select max(maas) from calisanlar where isyeri=marka_isim) as max_maas,
+(select min(maas) from calisanlar where isyeri=marka_isim) as min_maas from markalar;
 
 -- max ve min maas bir arada olsun
 
@@ -81,3 +75,5 @@ select isim, maas, isyeri from calisanlar where isyeri in(select marka_isim from
 -- SORU7: Her markanin id’sini, ismini ve toplam kaç şehirde bulunduğunu listeleyen bir SORGU yaziniz.
 
 -- count(sehir) sehirleri sayar
+
+select marka_id, marka_isim, (select count(distinct sehir) from calisanlar where isyeri=marka_isim) as top_sehir from markalar;
--- day05_2OrderBy.sql
+++ day05_2OrderBy.sql
@@ -76,11 +76,24 @@ select * from kisiler where id>10 and id<=12;
 select * from kisiler order by maas desc;
 select * from kisiler order by maas desc limit 3;
 
+/* Oracle SQL Çözümü
+select * from kisiler order by maas desc
+fetch next 3 rows only;
+*/
+
 
 -- MAAŞ'a göre sıralamada 4. 5.  6. kişilerin bilgilerini listeleyen sorguyu yazınız.
 select * from kisiler order by maas desc limit 3,3;
 -- ilk 3 rakami 4. degeri al demek
--- iknci 3 rakamda 4,5,6 y al
+-- iknci 3 rakamda 4,5,6 yi al.
+
+/* Oracle SQL Çözümü
+select * from kisiler order by maas desc
+OFFSET 3 ROWS			--> ilk 3 kaydı atladık
+FETCH NEXT 3 ROWS ONLY;	--> sonraki 3 kisiyi getir
+*/
+
 
 -- MAAŞ'a göre sıralamada 7. 8. 9. kişilerin bilgilerini listeleyen sorguyu yazınız.
 select * from kisiler order by maas desc limit 6,3;
+
